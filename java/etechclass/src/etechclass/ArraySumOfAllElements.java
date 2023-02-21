001b0f6a6868a6daed6906f06bc09447bc8d61dc
sarthak382 <sarthakdeep382@gmail.com>
2023-02-22 02:02:08 +0530

    8th code

---
+++ java/etechclass/src/etechclass/ArraySumOfAllElements.java
@@ -0,0 +1,14 @@
+package etechclass;
+
+public class ArraySumOfAllElements {
+    public static void main(String[] args){
+        int j = 0;
+        int arr[]={33,3,4,5};
+        for (int i = 0;  i< arr.length ; i++){
+            j = j + arr[i];
+        }
+        System.out.println(j);
+    }
+}
+
+
